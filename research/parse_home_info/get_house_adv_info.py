6d2bbbc0265a524774f6d9d5ae5d8fb406681a67
Charubaiel <aleksandrin.a@mail.ru>
2023-04-29 15:24:36 +0300

    refactor home_info

--- research/parse_home_info/get_house_adv_info.py
+++ research/parse_home_info/get_house_adv_info.py
@@ -1,30 +1,59 @@
 import pandas as pd
 from tqdm.contrib.concurrent import process_map
-from multiprocessing import Pool
+import pathlib
 
-base_url = 'https://dom.mingkh.ru'
-base_data = pd.read_csv('base_home_data.csv')
+ROOT = pathlib.Path(__file__).parent
+BASE_URL = 'https://dom.mingkh.ru'
+BASE_FILEPATH = 'base_home_data.csv'
 
 
-def parse_home_page(home_url):
-    full_url = base_url + home_url
+def parse_home_page(home_url: str):
+    full_url = BASE_URL + home_url
     _id = home_url.split('/')[-1]
     df_list = pd.read_html(full_url)
     home_data = pd.concat(df_list)\
-                        .iloc[:,:3]\
-                        .dropna(thresh=1)\
-                        .set_index(0)\
-                        .fillna(method='ffill',axis=1)\
-                        .iloc[:,1]\
-                        .rename(_id)\
-                        .rename_axis('id')
-    return home_data.to_frame().T.to_csv('complete_home_info.csv',mode='a')
-
-home_list = base_data['home_url'].where(lambda x: x!='home_url').dropna().drop_duplicates().tolist()
+        .iloc[:, :3]\
+        .dropna(thresh=1)\
+        .set_index(0)\
+        .fillna(method='ffill', axis=1)\
+        .iloc[:, 1]\
+        .rename(_id)\
+        .rename_axis('id')
+    return home_data
 
 
+
+def chunk_save(urls_to_load:list):
+    result = []
+    for idx,url in enumerate(urls_to_load):
+        result.append(parse_home_page(url))
+        if idx% 100 == 0 :
+            pd.concat(result,axis=1).to_parquet(f'{idx}_homes.parquet')
+            result = []
+            
+
+
+
+
+
+# def get_items_to_load(base_file: str, loaded_file: str):
+
+#     base_data = pd.read_csv(base_file)
+#     urls_to_load = set(base_data['home_url'].where(
+#         lambda x: x != 'home_url').dropna().drop_duplicates().tolist())
+    
+#     try:
+#         loaded_items = set(pd.concat([pd.read_parquet(i) for i in pathlib.Path(f'{ROOT}/home_parquet_data').glob('*')])['id'])
+#         urls_to_load = list(urls_to_load - loaded_homes)
+#         print(f'items to load : {len(urls_to_load)}')
+#     return urls_to_load
 
 
 if __name__ == "__main__":
-    process_map(parse_home_page,home_list,max_workers = 2,chunksize=100)    
+
+    process_map(chunk_save,
+                get_items_to_load(
+                                BASE_FILEPATH,
+                                ),
+                max_workers=2, chunksize=10)
 #     asyncio.run(make_requests(urls=home_list))
