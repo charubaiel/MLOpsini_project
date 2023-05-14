9cff48dd4c6311b78b8d1c75e01a4fe19c96ff3a
Charubaiel <aleksandrin.a@mail.ru>
2023-05-14 15:15:22 +0300

    add unified loggers

---
+++ src/utils/loggers.py
@@ -0,0 +1,20 @@
+import logging 
+from functools import wraps
+import datetime
+
+logging.basicConfig(format=u'#%(levelname)-s [%(name)s] [%(asctime)s]  %(message)s',
+                    level=logging.INFO)
+
+_logger = logging.getLogger('BASE')
+
+
+def logger(func):
+    @wraps(func)
+    def wrapper(*args,**kwargs):
+        start_time = datetime.datetime.now()
+        _logger.info(f'START {func.__name__}')
+        result = func(*args,**kwargs)
+        end_time = datetime.datetime.now() - start_time
+        _logger.info(f'END   {func.__name__} in {end_time}')
+        return result
+    return wrapper
